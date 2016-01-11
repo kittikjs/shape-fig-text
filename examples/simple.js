2f8f54b45605d363add8f8ce62887337eb2defae
ghaiklor <ghaiklor@gmail.com>
2016-01-11 16:18:32 +0200

    docs(shape): Update simple example

--- examples/simple.js
+++ examples/simple.js
@@ -4,10 +4,12 @@ const FigText = require('../lib/FigText').default;
 const cursor = require('kittik-cursor').Cursor.create().resetTTY();
 
 FigText.create({
-  text: 'KittikJS Rules',
-  y: 3,
-  font: 'Red Phoenix',
-  foreground: cursor.COLORS.WHITE
+  text: 'KittikJS\n    Rules\n           !!!',
+  x: 'center',
+  y: 'middle',
+  font: 'Star Wars',
+  foreground: 'yellow_1',
+  horizontalLayout: 'full'
 }).render(cursor);
 
 cursor.flush();
